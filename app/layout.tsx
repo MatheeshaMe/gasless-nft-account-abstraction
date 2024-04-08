86216094689533a3532e359774514b28ded6b9b0
Matheesha <hpraveenmatheesha@gmail.com>
2024-04-08 20:41:43 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Footer from "@/components/instructionsComponent/navigation/footer";
+import Navbar from "@/components/instructionsComponent/navigation/navbar";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { WagmiConfig, createConfig, sepolia } from "wagmi";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_API_KEY, // or infuraId
+    walletConnectProjectId: process.env.CONNECT_KIT_PROJECT_ID!,
+    chains: [sepolia],
+    // Required
+    appName: "aa-course",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's logo,no bigger than 1024x1024px (max. 1MB)
+  })
+);
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <WagmiConfig config={config}>
+        <ConnectKitProvider mode="dark">
+          <body>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "105vh",
+              }}
+            >
+              <Navbar />
+              <div style={{ flexGrow: 1 }}>{children}</div>
+              <Footer />
+            </div>
+          </body>
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </html>
+  );
+}
